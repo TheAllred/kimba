d5835b0c6783db1c8e060e96c1d0c8cc0e6f837a
Kevin Allred <kevinawesomeallred@gmail.com>
2024-05-18 10:50:09 -0600

    tailwind

--- kimba-app/app/root.tsx
+++ kimba-app/app/root.tsx
@@ -1,5 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -9,10 +7,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css";
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 export default function App() {
   return (
     <html lang="en">
